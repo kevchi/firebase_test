b73e7f28c9acccaeeed09b9c38c6f826e96f90b2
Kevin <kevchi@users.noreply.github.com>
2024-01-20 04:23:00 -0500

    updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,16 @@
+/** @format */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
-
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	signOut,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 // FIREBASE CONFIG MODULE
-import { firebaseConfig } from './firebaseConfig.js';
+import { firebaseConfig } from "./firebaseConfig.js";
 
 const app = initializeApp(firebaseConfig);
 
@@ -21,19 +21,17 @@ export const signInWithEmailAndPassword_Fi = signInWithEmailAndPassword;
 export const onAuthStateChanged_Fi = onAuthStateChanged;
 export const signOut_Fi = signOut;
 
-import { checkAuthState, userSignUp, userSignIn, userSignOut } from './auth.js';
-
+import { checkAuthState, userSignUp, userSignIn, userSignOut } from "./auth.js";
 
 checkAuthState();
-userSignUp() 
-userSignIn() 
-userSignOut()
+userSignUp();
+userSignIn();
+userSignOut();
 
 const signUpButton = document.querySelector("#signUpButton");
 const signInButton = document.querySelector("#signInButton");
 const signOutButton = document.querySelector("#signOutButton");
 
-
-signUpButton.addEventListener('click', userSignUp);
-signInButton.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignOut);
+signUpButton.addEventListener("click", userSignUp);
+signInButton.addEventListener("click", userSignIn);
+signOutButton.addEventListener("click", userSignOut);
--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,61 +1,82 @@
-import { auth, createUserWithEmailAndPassword_Fi, signInWithEmailAndPassword_Fi, onAuthStateChanged_Fi, signOut_Fi } from './app.js'
+/** @format */
 
-    export const checkAuthState = async() => {
-      onAuthStateChanged_Fi(auth, user => {
-          if(user) {
-              authForm.style.display = 'none';
-              secretContent.style.display = 'block';
-          }
-          else {
-              authForm.style.display = 'block';
-              secretContent.style.display = 'none';
-          }
-      })
-  }
-  
+import {
+	auth,
+	createUserWithEmailAndPassword_Fi,
+	signInWithEmailAndPassword_Fi,
+	onAuthStateChanged_Fi,
+	signOut_Fi,
+} from "./app.js";
 
+export const checkAuthState = async () => {
+	onAuthStateChanged_Fi(auth, (user) => {
+		if (user) {
+			document.querySelector("#nav-wrap").style.display = "block";
+			document.querySelector("#signUpForm").style.display = "none";
+			document.querySelector("#signInForm").style.display = "none";
+			document.querySelector("#transactions").style.display = "block";
+		} else {
+			document.querySelector("#nav-wrap").style.display = "none";
+			document.querySelector("#signInForm").style.display = "inline-block";
+			document.querySelector("#signUpForm").style.display = "inline-block";
+			document.querySelector("#transactions").style.display = "none";
+		}
+	});
+};
 
-const userEmail = document.querySelector("#userEmail");
-const userPassword = document.querySelector("#userPassword");
-const authForm = document.querySelector("#authForm");
+const signInEmail = document.querySelector("#signInEmail");
+const signInPassword = document.querySelector("#signInPassword");
+const signInForm = document.querySelector("#signInForm");
+
+const signUpEmail = document.querySelector("#signUpEmail");
+const signUpPassword = document.querySelector("#signUpPassword");
+const signUpForm = document.querySelector("#signUpForm");
+
 const secretContent = document.querySelector("#secretContent");
+const signOut = document.querySelector("#signOut");
 
+// export const userSignIn = document.querySelector("#LogInForm");
+// userSignIn.addEventListener(submit, (e)=> {
+//     e.preventDefault();
+//     const signInEmail = userSignIn['signInEmail'].value;
+//     const signInPassword = userSignIn['signInPassword'].value;
 
-secretContent.style.display = 'none';
+//     auth.signInWithEmailAndPassword_Fi(signInEmail, signInPassword).then(eventObj => {
+//         console.log(eventObj)
+//     })
+// })
 
-export const userSignUp = async() => {
-    const signUpEmail = userEmail.value;
-    const signUpPassword = userPassword.value;
-    createUserWithEmailAndPassword_Fi(auth, signUpEmail, signUpPassword)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        alert("Your account has been created!");
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage)
-    })
-}
+export const userSignIn = async () => {
+	const signInEmailEl = signInEmail.value;
+	const signInPasswordEl = signInPassword.value;
+	signInWithEmailAndPassword_Fi(auth, signInEmailEl, signInPasswordEl)
+		.then((userCredential) => {
+			const user = userCredential.user;
+			alert("You have signed in successfully!");
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode + errorMessage);
+		});
+};
 
-export const userSignIn = async() => {
-    const signInEmail = userEmail.value;
-    const signInPassword = userPassword.value;
-    signInWithEmailAndPassword_Fi(auth, signInEmail, signInPassword)
-    .then((userCredential) => {
-        const user = userCredential.user;
-        alert("You have signed in successfully!");
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage)
-    })
-}
+export const userSignUp = async () => {
+	const signUpEmailEl = signUpEmail.value;
+	const signUpPasswordEl = signUpPassword.value;
+	createUserWithEmailAndPassword_Fi(auth, signUpEmailEl, signUpPasswordEl)
+		.then((userCredential) => {
+			const user = userCredential.user;
+			console.log(user);
+			alert("Your account has been created!");
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode + errorMessage);
+		});
+};
 
-
-export const userSignOut = async() => {
-    await signOut_Fi(auth);
-}
-
+export const userSignOut = async () => {
+	await signOut_Fi(auth);
+};
